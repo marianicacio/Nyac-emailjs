8511c3cd85ed26de44fbb5b59e6d9c5994702301
Maria Eduarda <mariaeduarda.nicacio2007@gmail.com>
2024-06-29 09:23:31 -0300

    feat: email.js

--- src/service/email/scripts/receive-email.js
+++ src/service/email/scripts/receive-email.js
@@ -1,10 +1,10 @@
-(function () {
+(() => {
         emailjs.init({
             publicKey: "go_2-FWamsEeI4gr2",
         })
     })();
 
-    document.getElementById("contact-form").addEventListener("submit", function(e) {
+    document.getElementById("contact-form").addEventListener("submit", (e) => {
         e.preventDefault()
 
         emailjs.sendForm("service_s69h4or", "template_5c60fx8", this)
